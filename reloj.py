46ad61b5150616e02feed920430aa992a93a89b2
rociogsierra <113629813+rociogsierra@users.noreply.github.com>
2022-12-11 23:08:40 +0100

    hago ell ejercicio en tres formatos distintos

--- reloj.py
+++ reloj.py
@@ -26,7 +26,32 @@ import datetime
 #%Y - Numero de año entero (2014)
 #%Z - Zona horaria
 
+
+#PRIMERA FORMA DE HACER EL EJERCICIO
+ahora = datetime.datetime.now()
+print(ahora)
+print(type(ahora))
+print(ahora.strftime("%d/%m/%Y, %H:%M:%S"))
+
+
+#SEGUNDA FORMA DE HACER EL EJERCICIO
+horario = datetime.now()
+fecha = horario.strftime("%d/%m/%Y, %H:%M:%S")
+print(fecha)
+#aquí añadiré "sleep" y "system", propuestos en el enunciado
+time.sleep(secs = 4)
+os.system("cls")
+
+
+#TERCERA FORMA DE HACER EL EJERCICIO
 x = datetime.datetime.now()
+x.hour
+x.minute
+x.year
+x.day
+x.month
+x.second
+
 print("reloj de horas minutos y segundos: ")
 
 print ("Hora = %s" %x.hour)
@@ -44,4 +69,15 @@ print ("Formato hh:mm:ss = %s:%s:%s" % (x.hour, x.month, x.second))
 #esta función devolverá lo mismo pero en otro formato
 #ejemplo de lo que devolverá esta función:
 #Formato hh:mm:ss = 12:10:50
+
+#No lo pide el enunciado, pero añadiré año, mes y día para completar el ejercicio
+print ("Año = %s" %x.year)
+print ("Mes = %s" %x.month)
+print ("Dia =  %s" %x.day)
+
+#ejemplo de lo que devolverá esta función:
+#Año = 2022
+#Mes = 12
+#Dia = 11
+
 
