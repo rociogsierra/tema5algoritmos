69ed57c6a7834b03d65fcd991c9f217ec805ee72
rociogsierra <113629813+rociogsierra@users.noreply.github.com>
2022-12-11 22:37:57 +0100

    informacion que pondré en el código

---
+++ reloj.py
@@ -0,0 +1,28 @@
+
+import time
+import os
+import datetime
+
+#PARA EJECUTARLO TENGO EN CUENTA:
+#%a - Nombre del día de la semana
+#%A - Nombre del día completo
+#%b - Nombre abreviado del mes
+#%B - Nombre completo del mes
+#%c - Fecha y hora actual
+#%d - Día del mes
+#%H - Hora (formato 24 horas)
+#%I - Hora (formato 12 horas)
+#%j - Día del año
+#%m - Mes en número
+#%M- Minutos
+#%p - Equivalente de AM o PM
+#%S - Segundos
+#%U - Semana del año (domingo como primer día de la semana)
+#%w - Día de la semana
+#%W - Semana del año (lunes como primer día de la semana)
+#%x - Fecha actual
+#%X - Hora actual
+#%y - Número de año (14)
+#%Y - Numero de año entero (2014)
+#%Z - Zona horaria
+
